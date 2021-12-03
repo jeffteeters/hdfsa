f839101b87a631c393a69545caebb6083fe76ac9
jeff Teeters <jeff@teeters.us>
2021-12-03 07:25:35 -0800

    compute expected noise using mixture

--- overlap.py
+++ overlap.py
@@ -12,11 +12,13 @@ class Env:
 	# command line arguments
 	parms = [
 		{ "name":"num_states", "kw":{"help":"Number of states (items) stored in sdm", "type":int},
-	 	  "flag":"s", "required_init":"i", "default":20 },
+	 	  "flag":"s", "required_init":"i", "default":1000 },
 	 	{ "name":"num_rows", "kw":{"help":"Number rows in sdm memory","type":int},
-	 	  "flag":"m", "required_init":"i", "default":2048 },
+	 	  "flag":"m", "required_init":"i", "default":1939 }, # 2048
 		{ "name":"activation_count", "kw":{"help":"Number memory rows to activate for each address (sdm)","type":int},
-		  "flag":"a", "required_init":"m", "default":20},
+		  "flag":"a", "required_init":"m", "default":19},
+		{ "name":"noise_percent", "kw":{"help":"Percent of counters to flip in memory to test noise resiliency",
+		  "type":float}, "flag":"n", "required_init":"m", "default":0.0},
 		{ "name":"seed", "kw":{"help":"Random number seed","type":int},
 		  "flag":"e", "required_init":"i", "default":2021},
 		{ "name":"debug", "kw":{"help":"Debug mode","type":int, "choices":[0, 1]},
@@ -75,6 +77,7 @@ class SDM_tester():
 			print("item_values\n%s" % self.item_values)
 			print("item_rows\n%s" % self.item_rows)
 		self.save_items()
+		self.add_noise()
 		self.recall_items()
 
 	def save_items(self):
@@ -86,6 +89,13 @@ class SDM_tester():
 				print("after saving %i, counters=%s" % (i, self.sdm_counters))
 		if self.pvals["debug"]:
 			print("sdm_counters\n%s" % self.sdm_counters)
+
+	def add_noise(self):
+		self.number_to_flip = round(self.pvals["noise_percent"] * self.pvals["num_rows"] / 100.0)
+		if self.number_to_flip == 0:
+			return
+		indicies = np.random.choice(self.pvals["num_rows"], size=self.number_to_flip, replace=False)
+		self.sdm_counters[indicies] *= -1
 
 	def recall_items(self):
 		recalled_values = np.empty(self.pvals["num_states"], dtype=np.int8)
@@ -101,6 +111,19 @@ class SDM_tester():
 		hamming_if_512 = 512 * ((self.pvals["num_states"] - num_correct) / self.pvals["num_states"])
 		print("num_correct = %s / %s (%s%%), expected=%s%%, hamming_if_512=%s" % (num_correct, self.pvals["num_states"], 
 			round(num_correct * 100.0 / self.pvals["num_states"], 1), percent_expected_correct, hamming_if_512))
+		if self.number_to_flip > 0:
+			fraction_wrong = (100.0 - percent_expected_correct) / 100.0
+			delta_ht = ( 1.0 - 2.0 * fraction_wrong) * self.pvals["noise_percent"] / 100.0
+			total_fraction_wrong = fraction_wrong + delta_ht
+			noise_percent_expected_correct = (1.0 - total_fraction_wrong) * 100.0
+			noise_hamming_if_512 = 512 * (1.0 - total_fraction_wrong)
+			print("with %s%% noise, expect %s%% correct, expected hamming_if_512=%s" % (
+				self.pvals["noise_percent"], noise_percent_expected_correct, noise_hamming_if_512))
+			percent_expected_correct_with_noise = self.compute_percent_expected_correct_with_noise()
+			print("Using changed mean, expected is: %s" % percent_expected_correct_with_noise)
+			percent_expected_correct_with_noise_via_single_mixture = self.compute_percent_expected_correct_with_noise_via_single_mixture()
+			print("Using single_misture, expected is: %s" % percent_expected_correct_with_noise_via_single_mixture)
+
 		if self.pvals["debug"]:
 			print("stored values, recalled values=\n%s\n%s" % (self.item_values, recalled_values))
 
@@ -115,6 +138,44 @@ class SDM_tester():
 		percent_expected_correct = round((1 - probability_single_bit_failure) * 100.0, 1)
 		return percent_expected_correct
 
+
+	def compute_distribution_for_specific_nact(self, nact):
+		# compute mean and standard distribution for a specific activation count
+		pact = nact / self.pvals["num_rows"]
+		mean = nact
+		num_entities_stored = self.pvals["num_states"]
+		sdm_num_rows = self.pvals["num_rows"]
+		variance = mean * (1 + pact * num_entities_stored * (1.0 + pact*pact * sdm_num_rows))
+		return (mean, variance)
+
+	def compute_percent_expected_correct_with_noise_via_single_mixture(self):
+		# use single mixture distributions to compute theoretical sdm bit error
+		fraction_flipped = self.pvals["noise_percent"] / 100.0
+		fraction_upright = 1.0 - fraction_flipped
+		nact_flipped = self.pvals["activation_count"] * fraction_flipped
+		nact_upright = self.pvals["activation_count"] - fraction_upright
+		mean_flipped, variance_flipped = self.compute_distribution_for_specific_nact(nact_flipped)
+		mean_upright, variance_upright = self.compute_distribution_for_specific_nact(nact_upright)
+		mean_combined = fraction_upright * mean_upright - fraction_flipped * mean_flipped
+		variance_combined = (fraction_upright * (variance_upright + mean_upright**2)
+			+ fraction_flipped * (variance_flipped + mean_flipped**2)) - mean_combined
+		standard_deviation_combined = math.sqrt(variance_combined)
+		probability_single_bit_failure = norm(0, 1).cdf(-mean_combined/standard_deviation_combined)
+		percent_expected_correct = round((1 - probability_single_bit_failure) * 100.0, 1)
+		return percent_expected_correct
+
+
+	def compute_percent_expected_correct_with_noise(self):
+		# compute theoretical sdm bit error based on Pentti's book chapter, but change mean because of noise
+		pact = self.pvals["activation_count"] / self.pvals["num_rows"]
+		mean = self.pvals["activation_count"]
+		mean = mean * (1 - 2*(self.pvals["noise_percent"] / 100.0))
+		num_entities_stored = self.pvals["num_states"]
+		sdm_num_rows = self.pvals["num_rows"]
+		standard_deviation = math.sqrt(mean * (1 + pact * num_entities_stored * (1.0 + pact*pact * sdm_num_rows)))
+		probability_single_bit_failure = norm(0, 1).cdf(-mean/standard_deviation)
+		percent_expected_correct_with_noise = round((1 - probability_single_bit_failure) * 100.0, 1)
+		return percent_expected_correct_with_noise
 
 class Hit_Counter():
 	# counts number of hits of each row in simulation of storing in sdm
