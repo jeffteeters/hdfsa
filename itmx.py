fcd159c1ed275df2c92ff5b06c09ab54567af1f9
jeff Teeters <jeff@teeters.us>
2021-12-28 15:48:33 -0600

    fix errors, got working to make error plots displayed

--- itmx.py
+++ itmx.py
@@ -4,6 +4,7 @@ import numpy as np
 import argparse
 import matplotlib.pyplot as plt
 import sys
+import statistics
 
 class Env:
 	# stores environment settings and data arrays
@@ -14,14 +15,16 @@ class Env:
 	 	  "flag":"s", "required_init":"i", "default":100 },
 	 	{ "name":"word_length", "kw":{"help":"Word length for item memory, 0 to disable", "type":int},
 	 	  "flag":"w", "required_init":"i", "default":512 },
+	 	{ "name":"num_trials", "kw":{"help":"Number of trials to run (used when generating table)", "type":int},
+	 	  "flag":"t", "required_init":"i", "default":3 },
 	 	{ "name":"loop_start", "kw":{"help":"Start value for loop","type":int},
 		  "flag":"x", "required_init":"", "default":0},
 		{ "name":"loop_step", "kw":{"help":"Step value for loop","type":int},
-		  "flag":"y", "required_init":"", "default":1},
+		  "flag":"y", "required_init":"", "default":5},
 		{ "name":"loop_stop", "kw":{"help":"Stop value for loop","type":int},
-		  "flag":"z", "required_init":"", "default":10},
-		{ "name":"values_to_plot", "kw":{"help":"Values to plot.  l-loop variable, e-prob error","type":str, "choices":["l", "e"]},
-		  "flag":"p", "required_init":"", "default":"l"},
+		  "flag":"z", "required_init":"", "default":50},
+		{ "name":"values_to_plot", "kw":{"help":"Values to plot.  l-loop variable, e-prob error","type":str,
+			"choices":["l", "e"]}, "flag":"p", "required_init":"", "default":"l"},
 		{ "name":"seed", "kw":{"help":"Random number seed","type":int},
 		  "flag":"e", "required_init":"i", "default":2021},
 		{ "name":"debug", "kw":{"help":"Debug mode","type":int, "choices":[0, 1]},
@@ -106,21 +109,28 @@ class Calculator:
 	def __init__(self, env):
 		self.env = env
 		self.figures = []
+		start = self.env.pvals["loop_start"]
+		step = self.env.pvals["loop_step"]
+		stop = self.env.pvals["loop_stop"]
+		self.xvals = list(range(start, stop, step))
 		values_to_plot = self.env.pvals["values_to_plot"]
 		if values_to_plot == "l":
 			# loop values
 			self.calculate_loop_values()
 			self.calculate_loop_values(0.5)
+		elif values_to_plot == "e":
+			self.calculate_item_memory_match_values()
 		else:
 			sys.exit("values_to_plot '%s' not implemented" % values_to_plot)
 
+	def show_plots(self):
+		for i in range(len(self.figures)):
+			print("plotting # %s" % i)
+			self.figures[i].make_plot(i+1)
+
 	def calculate_loop_values(self, factor=0.75):
-		start = self.env.pvals["loop_start"]
-		step = self.env.pvals["loop_step"]
-		stop = self.env.pvals["loop_stop"]
-		xvals = list(range(start, stop, step))
-		yvals = xvals
-		fig = Figure(title="Loop variable %s" % factor, xvals=xvals, grid=True,
+		yvals = self.xvals
+		fig = Figure(title="Loop variable %s" % factor, xvals=self.xvals, grid=True,
 			xlabel="loop variable value", ylabel="loop var *factor", xaxis_labels=None,
 			legend_location="lower right",
 			yvals=yvals, ebar=None, legend="loop var")
@@ -128,9 +138,77 @@ class Calculator:
 		fig.add_line(yvf, ebar=None, legend="loop var*%s" % factor)
 		self.figures.append(fig)
 
-	def show_plots(self):
-		for i in range(len(self.figures)):
-			self.figures[i].make_plot(i+1)
+	def hamming(self, a, b):
+		# return hamming distance between binary vectors a and b
+		return np.count_nonzero(a!=b)
+
+	def calculate_item_memory_match_values(self):
+		num_items = self.env.pvals["num_items"]
+		word_length = self.env.pvals["word_length"]
+		item_memory = np.random.randint(0, high=2, size=(num_items, word_length), dtype=np.int8)
+		num_matches_correct_all = []
+		num_matches_tried_all = []
+		hamming_to_closest_distractor_all = []
+		rng = np.random.default_rng()
+		for xval in self.xvals:
+			number_bits_to_flip = int(word_length * num_items * xval / 100.0)  # convert from percent to number of bits
+			if self.env.pvals["debug"]:
+				print("xval=%s, number_bits_to_flip=%s" % (xval, number_bits_to_flip))
+			num_matches_correct = 0
+			num_matches_tried = 0
+			hamming_to_closest_distractor = []
+			for trial in range(self.env.pvals["num_trials"]):
+				corrupted_items = item_memory.flatten()
+				bits_to_flip = rng.choice(word_length* num_items, size=number_bits_to_flip, replace=False, shuffle=False)
+				corrupted_items[bits_to_flip] = 1 - corrupted_items[bits_to_flip]
+				corrupted_items.resize( (num_items, word_length) )
+				for item_id in range(num_items):
+					item_hamming = self.hamming(corrupted_items[item_id], item_memory[item_id])
+					if self.env.pvals["debug"]:
+						print("item_id=%s, corrupted_item=%s, item=%s, item_hamming=%s" % (item_id, corrupted_items[item_id],
+							item_memory[item_id], item_hamming))
+					min_distractor_hamming = word_length
+					for did in range(num_items):  # distractor_id
+						if did == item_id:
+							continue
+						distractor_hamming = self.hamming(corrupted_items[item_id], item_memory[did])
+						if self.env.pvals["debug"]:
+							print("did=%s, distractor_hamming=%s" % (did, distractor_hamming))
+						if distractor_hamming < min_distractor_hamming:
+							min_distractor_hamming = distractor_hamming
+					num_matches_tried += 1
+					if item_hamming < min_distractor_hamming:
+						num_matches_correct += 1
+					hamming_to_closest_distractor.append(min_distractor_hamming)
+				if self.env.pvals["debug"]:
+					print("Hamming item=%s, min_distractor_hamming=%s" % (item_hamming, min_distractor_hamming))
+			num_matches_correct_all.append(num_matches_correct)
+			num_matches_tried_all.append(num_matches_tried)
+			mean_hamming_to_closest_distractor = statistics.mean(hamming_to_closest_distractor)
+			stdev_hamming_to_closest_distractor = statistics.stdev(hamming_to_closest_distractor)
+			hamming_to_closest_distractor_all.append((mean_hamming_to_closest_distractor,
+					stdev_hamming_to_closest_distractor))
+			if self.env.pvals["debug"]:
+				print("hamming_to_closest_distractor=%s" % hamming_to_closest_distractor)
+				print("mean=%s, stdev=%s" % (mean_hamming_to_closest_distractor, stdev_hamming_to_closest_distractor))
+				import pdb; pdb.set_trace()
+		# make plots
+		num_xvals = len(self.xvals)
+		yvals = [(num_matches_tried_all[i] - num_matches_correct_all[i])*100.0/num_matches_tried_all[i] for i in range(num_xvals)]
+		fig = Figure(title="Recall error vs bit flips", xvals=self.xvals, grid=True,
+			xlabel="bit flips (%)", ylabel="recall error (percent)", xaxis_labels=None,
+			legend_location="lower right",
+			yvals=yvals, ebar=None, legend="recall error")
+		self.figures.append(fig)
+		yham = [hamming_to_closest_distractor_all[i][0] * 100.0 / word_length for i in range(num_xvals)]
+		yhamebar = [hamming_to_closest_distractor_all[i][1] * 50.0 / word_length for i in range(num_xvals)]
+		fig = Figure(title="Distractor hamming % vs bit flips", xvals=self.xvals, grid=True,
+			xlabel="bit flips (%)", ylabel="Hamming difference (percent)", xaxis_labels=None,
+			legend_location="lower right",
+			yvals=yham, ebar=yhamebar, legend="Distractor hamming diff (%)")
+
+		# fig.add_line(yham, ebar=yhamebar, legend="Distractor hamming diff (%)")
+		self.figures.append(fig)
 
 
 def main():
