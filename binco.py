abec97a9e0554c3f293fd19834ffb0eea31c691b
jeff Teeters <jeff@teeters.us>
2022-06-20 21:34:52 -0500

    add more code to binco.py

--- binco.py
+++ binco.py
@@ -15,14 +15,16 @@ class Binco:
 	# calculate delta (normalized hamming distance) when there are
 	# coordinated overlaps (onto multiple counters) of a binarized sdm.
 
-	def __init__(self, nrows, nact, k, novl, epochs=10, debug=False):
+	def __init__(self, nrows, nact, k, novl, delt_overlap, epochs=10, debug=False):
 		# nrows - number of rows in sdm
 		# nact - activaction count
 		# k - number of items (transitions) to store in sdm
 		# novl - int arry, stores number of overlaps (including target) of each selected row
+		# delt_overlap - normalized hamming distance for number of overlaps without? counting target, indexed by overlaps
 		# epochs - used for testing
 		assert nact > 1
 		assert novl.size == nact
+		assert np.amin(novl) > 0, "must be at least target stored in counter"
 		self.nrows = nrows
 		self.nact = nact
 		self.k = k
@@ -32,14 +34,67 @@ class Binco:
 		rng = np.random.default_rng()
 		self.co_probs = self.calculate_co_probs()  # calculate probabilities of coordinated overlaps
 		# print("co_probs=%s" % self.co_probs)
-		son = novl.copy()   # single overlap number.  Will be modified when coordinated overlaps added
+		son = novl.copy()-1 # single overlap number.  Will be modified when coordinated overlaps added. -1 for target
 		nco_added = 0       # number coordinated overlaps added
 		ave_num_overlaps = k * nact / nrows
 		max_num_coordinated_overlaps = max(10, 2*round(ave_num_overlaps))
-		co_ptr = np.zeros((nact-1, max_num_coordinated_overlaps), dtype = np.uint16)  # has index of values in covals
+		co_ptr = np.full((nact-1, max_num_coordinated_overlaps), -1, dtype = np.int16)  # has index of values in covals;
+			# initialize with -1 so can see more easily when values filled in
 		covals = np.zeros( (nact-1)* max_num_coordinated_overlaps, dtype =np.int8)  # has values of coordinated overlaps, +-1
 		num_covals = 0  # number coordinated overlaps added (length of valid covals)
 		num_co_ptr = np.zeros(nact-1, dtype=np.uint16)  # has number of values in each row of co_ptr
+		# potentially create coordinated overlaps of different sizes, starting from largest size down
+		for nc in range(nact, 1, -1):  # number in overlap from same item
+			seli = list(itertools.combinations(range(nact), nc))  # counters to compare
+			for si in seli:
+				min_length = np.amin(son[si])
+				num_co_to_add = binom.rvs(min_length, co_probs[nc-2])
+				for j in num_co_to_add:
+					for i in si:
+						co_ptr[i, num_co_ptr[i]] = num_covals
+						num_co_ptr[i] += 1
+					num_covals += 1
+					son[si] -= 1   # reduce number of single overlaps by one, since one was used
+		# now, calculate overall delta using configuration with added overlaps
+		if num_covals == 0:
+			# no coordinated values added, return normal voting calculation, same as in binarized_sdm_analytical
+			pc_er = np.empty(2*nact, dtype=np.float64)  # for holding probability correct then probability error 
+			er = delt_overlap[son]  # probability error
+			pc = 1 - er # probability correct
+			pc_er[0:nact] = pc  # copy probability correct
+			pc_er[nact:] = er   # copy probability error
+			pvote = np.sum(np.prod(pc_er[mpc], axis=1))
+			delta = 1 - pvote
+		else:
+			# compute response to every combination of overlap values
+			sv = list(itertools.combinations(range(nact), nc))  # counters to compare
+
+
+
+				10e6  # larger than number in any counter
+				for hli in seli[si]:  # hard location index
+					cl = len(son[hli])
+					if cl < min_length:
+						min_length = cl
+						# min_length is the maximum number of overlaps of length nc
+						# now find actual number of overlaps
+						cs = sdm_rcp[seli[si][0]]
+						for hli in seli[si][1:]:  # hard location index
+							cs = cs.intersection(sdm_rcp[hli])
+						found_common = len(cs)
+						assert found_common >= 0
+						assert min_length >= 0
+						assert min_length >= found_common
+						checked_found[nc-2]['checked'] += min_length
+						checked_found[nc-2]['found'] += found_common
+						# remove found_common from columns so not counted again
+						if found_common > 1:
+							for hli in seli[si]:  # hard location index
+								sdm_rcp[hli].difference_update(cs)
+		self.check_found = checked_found
+
+
+
 
 	def calculate_co_probs(self):
 		co_probs = np.empty(self.nact-1, dtype=np.float64)
