633853b8e12a54185c1cccc476e0642bb1b855fa
jeff Teeters <jeff@teeters.us>
2022-06-25 18:12:22 -0500

    script to parse and plot sdm_err_vs_nrows

---
+++ parse_output2.py
@@ -0,0 +1,137 @@
+
+# script to parse output of plot_sdm_err_vs_nrows and plot values
+
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import sys
+bits_per_counter=1
+
+# file_name = "archive/2022_06_plots/2022_06_10/sdm_bc1_nrows50_to_400-nact_1,3,5,7_vs.txt"
+file_name = "archive/2022_06_plots/2022_06_23/binarized_sdm_etest.txt"
+
+with open(file_name) as f:
+	output = f.read()
+
+# pattern = re.compile(r'[^\n]*\nstarting nrows=(\d+), nact=(\d), epochs=(\d+),\n(?:prob[^\n]+\n)?bsm_err=([^,]+),'
+# 	' emp_err=([^,]+), std=([^\n]+)\n')
+
+# pat =re.compile(r'[^\n]*\nstarting nrows=(\d+), nact=(\d), epochs=(\d+),[^\n]*?\nbsm_err=([^,]+), emp_err=([^,]+), std=([^\n]+)\n')
+# pat3 =re.compile(r'[^\n]*\nstarting nrows=(\d+), nact=(\d), epochs=(\d+),\nbsm_err=([^,]+), emp_err=([^,]+), std=([^\n]+)\n')
+pat2=re.compile(r'[^\n]*\nstarting nrows=(\d+), nact=(\d), epochs=(\d+),\s*\n(?:prob[^\n]+\n)?bsm_err=([^,]+), emp_err=([^,]+), std=([^\n]+)\n')
+pat3=re.compile(r'[^\n]*\nperr=(\d+), starting nrows=(\d+), nact=(\d), epochs=(\d+),\s*\n'
+	'bsm_err=([^,]+), emp_err=([^,]+), std=([^\n]+)\n')
+
+# nacts = [1,3,5,7]
+k=1000
+# rows_to_test = list(range(50, 421, 20))
+rows = []
+nacts = []
+emp_mean = []
+emp_clm = []
+bsm_err = []
+
+# emp_mean=np.empty((len(nacts), len(rows_to_test)), dtype=np.float64)
+# emp_clm=np.empty((len(nacts), len(rows_to_test)), dtype=np.float64)
+# bsm_err=np.empty((len(nacts), len(rows_to_test)), dtype=np.float64)
+# anl_err=np.empty((len(nacts), len(rows_to_test)), dtype=np.float64)
+
+for (perr, nrows, nact, epochs, bsm_er, emp_err, std) in re.findall(pat3, output):
+	print("found nrows=%s" % nrows)
+	perr = int(perr)
+	nrows = int(nrows)
+	nact = int(nact)
+	epochs = int(epochs)
+	bsm_er = float(bsm_er)
+	emp_err=float(emp_err)
+	std=float(std)
+
+	rows.append(nrows)
+	nacts.append(nact)
+	emp_mean.append(emp_err)
+	emp_clm.append((std / math.sqrt(epochs)) * 1.96)
+	bsm_err.append(bsm_er)
+
+show_ratios = False
+if show_ratios:
+	# display ratio of predicted to empirical
+	for i in range(len(nacts)):
+		print("nact=%s" % nacts[i])
+		for j in range(len(rows_to_test)):
+			print("rows=%s, empirical=%s, predicted=%s, ratio=%s" % (rows_to_test[j], emp_mean[i,j], bsm_err[i,j],
+				round(bsm_err[i,j]/emp_mean[i,j], 3)))
+	sys.exit("done for now")
+
+
+def add_correction_factor(nact, emp_err, bsm_er):
+	global nacts, bsm_err
+	ratio = emp_err/ bsm_er
+	naidx = nacts.index(nact)
+	bsm_err[naidx,:] *= ratio
+
+add_correction_factors = False
+if add_correction_factors:
+	nact=7; empirical_nact7_190=0.0008225; predicted_nact7_190=0.0002382145957503523
+	add_correction_factor(nact, empirical_nact7_190, predicted_nact7_190)
+	nact=5; empirical_nact5_190=0.0004025; predicted_nact5_190=0.00020955847080339485
+	add_correction_factor(nact, empirical_nact5_190, predicted_nact5_190)
+	nact=3; empirical_nact3_190=0.00021999999999999998; predicted_nact3_190=0.00019047177705489027
+	add_correction_factor(nact, empirical_nact3_190, predicted_nact3_190)
+
+# plot info
+# make plots
+plots_info = [
+	{"subplot": 121, "scale":"linear"},
+	{"subplot": 122, "scale":"log"},
+]
+
+# import pdb; pdb.set_trace()
+xvals = rows
+xaxis_labels = ["%s/%s" % (rows[i], nacts[i]) for i in range(len(rows))]
+for pi in plots_info:
+	plt.subplot(pi["subplot"])
+	plt.errorbar(xvals, emp_mean, yerr=emp_clm, fmt="-o", label="empirical")
+	plt.errorbar(xvals, bsm_err, yerr=None, fmt="-o", label="bsm predicted")
+	title = "SDM error rate vs nrows for binarized sdm"
+	if pi["scale"] == "log":
+		plt.yscale('log')
+		title += " (log scale)"
+	plt.xticks(xvals,xaxis_labels)
+	# plt.xticks(xvals, xticks)
+	plt.title(title)
+	plt.xlabel("SDM nrows")
+	plt.ylabel("fraction error")
+	plt.grid()
+	plt.legend(loc="upper right")
+plt.show()
+
+
+    # print("nrows=%s, nact=%s, epochs=%s, bsm_err=%s, emp_err=%s, std=%s" % (
+    # 	nrows, nact, epochs, bsm_err, emp_err, std))
+
+"""
+(base) Jeffs-MacBook:hdfsa jt$ time python plot_sdm_err_vs_nact.py
+starting nrows=50, nact=1, epochs=400,
+bsm_err=0.09744423457541718, emp_err=0.10477, std=0.009384940063740417
+
+starting nrows=50, nact=3, epochs=400,
+prob_one_trial_overlap=0.06, peak_num_overlaps=59
+bsm_err=0.18385813235924686, emp_err=0.21645000000000003, std=0.01183353286216758
+
+starting nrows=50, nact=5, epochs=400,
+prob_one_trial_overlap=0.1, peak_num_overlaps=100
+bsm_err=0.21097715330018152, emp_err=0.3125775, std=0.013006498135547486
+
+starting nrows=50, nact=7, epochs=400,
+prob_one_trial_overlap=0.14, peak_num_overlaps=139
+bsm_err=0.2233438542899311, emp_err=0.4227575, std=0.01584199147045598
+
+starting nrows=70, nact=1, epochs=400,
+bsm_err=0.03434493679186701, emp_err=0.03728, std=0.006688916205186009
+
+starting nrows=70, nact=3, epochs=400,
+prob_one_trial_overlap=0.04285714285714286, peak_num_overlaps=42
+bsm_err=0.07146743000055866, emp_err=0.087455, std=0.008223622985035246
+"""
+
