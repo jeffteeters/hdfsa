8568f63fdde2bd5564bf8ce1afe8b03777c28cb9
jeff Teeters <jeff@teeters.us>
2022-06-27 09:58:01 -0500

    use mdims dict to store dimensions

--- curve_fit.py
+++ curve_fit.py
@@ -11,87 +11,72 @@ from scipy.stats import linregress
 import numpy as np
 import sympy as sym
 
-
-bundle_dims_k1000_d100 = [
-	[1,24002],
-	[2, 40503],
-	[3, 55649],
-	[4, 70239],
-	[5, 84572],
-	[6, 98790],
-	[7, 112965],
-	[8, 127134],
-	[9, 141311],
-	]
-
-sdm_binarized_nact_optimum_dims = [
-	[1, 50, 1,], # 200],  # comment out epochs
-	[2, 97, 1,], #  200],
-	[3, 158, 3,], #  300],
-	[4, 208, 3,], #  400],
-	[5, 262, 3,], # 3000],
-	[6, 315, 5,], # 10000],
-	[7, 368, 5,], # 40000]]
-	[8, 424, 7,],
-	[9, 476, 7]]
-
-#output from sdm_anl.Sdm_error_analytical:
-# SDM sizes, nact:
-sdm_8bit_counter_dims = [
-	[1, 51, 1],
-	[2, 86, 2],
-	[3, 125, 2],
-	[4, 168, 2],
-	[5, 196, 3],
-	[6, 239, 3],
-	[7, 285, 3],
-	[8, 312, 4],
-	[9, 349, 4]]
+mdims = {
+	"bun_k1000_d100":
+		[[1,24002],
+		[2, 40503],
+		[3, 55649],
+		[4, 70239],
+		[5, 84572],
+		[6, 98790],
+		[7, 112965],
+		[8, 127134],
+		[9, 141311]],
+	"sdm_k1000_d100_c1": # sdm_binarized_nact_optimum_dims
+		[[1, 50, 1,], # 200],  # comment out epochs
+		[2, 97, 1,], #  200],
+		[3, 158, 3,], #  300],
+		[4, 208, 3,], #  400],
+		[5, 262, 3,], # 3000],
+		[6, 315, 5,], # 10000],
+		[7, 368, 5,], # 40000]]
+		[8, 424, 7,],
+		[9, 476, 7]],
+	"sdm_k1000_d100_c8":
+		#output from sdm_anl.Sdm_error_analytical:
+		# SDM sizes, nact:
+		# sdm_8bit_counter_dims = [
+		[[1, 51, 1],
+		[2, 86, 2],
+		[3, 125, 2],
+		[4, 168, 2],
+		[5, 196, 3],
+		[6, 239, 3],
+		[7, 285, 3],
+		[8, 312, 4],
+		[9, 349, 4]]
+	}
 
 class Line_fit():
 	# fit line to size of sdm or bundle
 
 	def __init__(self, mem_type):
-		assert mem_type in ("bundle_k1000_d100", "binarized_sdm", "8bit_counter_sdm")  # add other options here
-		if mem_type == "bundle":
-			x = np.empty(len(bundle_dims), dtype=np.float64)
-			y = np.empty(len(bundle_dims), dtype=np.float64)
-			for i in range(len(bundle_dims)):
-				perr, width= bundle_dims[i]
-				assert perr == i + 1
-				x[i] = width
-				y[i] = 10**(-perr)
-
-		elif mem_type in ("binarized_sdm","8bit_counter_sdm"):
-			sdm_dims = sdm_binarized_nact_optimum_dims if mem_type == "binarized_sdm" else sdm_8bit_counter_dims
-			x = np.empty(len(sdm_dims), dtype=np.float64)
-			y = np.empty(len(sdm_dims), dtype=np.float64)
-			for i in range(len(sdm_dims)):
-				perr, nrows, nact = sdm_dims[i]
-				assert perr == i + 1
-				x[i] = nrows
-				y[i] = 10**(-perr)
-		else:
-			sys.exit("invalid dims specified: %s" % dims_using)
+		if mem_type not in mdims:
+			print("Invalid mem_type: %s" %(mem_type))
+			sys.exit("options are: %s" % sorted(mem_type.keys()))
+		dims = mdims[mem_type]
+		nsizes = len(dims)
+		x = np.empty(nsizes, dtype=np.float64)
+		y = np.empty(nsizes, dtype=np.float64)
+		for i in range(nsizes):
+			perr, width = dims[i][0:2]
+			assert perr == i + 1
+			x[i] = width
+			y[i] = 10**(-perr)
 		ylog = np.log(y)  # store log of error for regression
-
 		result = linregress(x, y=ylog)
-		# print("%s linregress slope=%s, intercept=%s, exp intercept=%s" % (mem_type,
-		# 	result.slope, result.intercept, np.exp(result.intercept)))
-		k = np.exp(result.intercept)
-		m = -result.slope
+		self.k = np.exp(result.intercept)
+		self.m = -result.slope
 		self.x = x
 		self.y = y
-		self.yreg = k * np.exp(-m *x)
-		self.k = k
-		self.m = m
-		print("%s for err=k*exp(-m*x), k=%s, m=%s" % (mem_type, k, m))
+		self.yreg = self.k * np.exp(-self.m *x)
+		print("%s for err=k*exp(-m*x), k=%s, m=%s" % (mem_type, self.k, self.m))
 
 
 def plot_sdm_vs_bundle():
-	bundle = Line_fit("bundle")
-	bin_sdm = Line_fit("binarized_sdm")
-	full_sdm = Line_fit("8bit_counter_sdm")
+	bundle = Line_fit("bun_k1000_d100")
+	bin_sdm = Line_fit("sdm_k1000_d100_c1") #("binarized_sdm")
+	full_sdm = Line_fit("sdm_k1000_d100_c8") # ("8bit_counter_sdm")
 	bin_sdm_bits = bin_sdm.x * 512  # bits per row
 	full_sdm_bits = full_sdm.x * 512 * 8
 
