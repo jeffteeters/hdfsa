8c09adf9ff48fd01b485ef6ca2115f65207a0560
jeff Teeters <jeff@teeters.us>
2022-07-01 16:00:48 -0500

    got working with non-binarized bundles

--- fast_bundle_empirical.py
+++ fast_bundle_empirical.py
@@ -64,9 +64,10 @@ class Fast_bundle_empirical():
 		trial_count = 0
 		fail_count = 0
 		for epoch_id in range(self.epochs):
-			im_action = rng.integers(0, high=2, size=(self.actions, self.ncols), dtype=np.int8)*2-1
+			# import pdb; pdb.set_trace()
+			im_action = rng.integers(0, high=2, size=(self.actions, self.ncols), dtype=np.int8)
 			# im_action = np.random.randint(0,high=2,size=(self.actions, self.ncols)).astype(np.int8)
-			im_state = rng.integers(0, high=2, size=(self.states, self.ncols), dtype=np.int8)*2-1
+			im_state = rng.integers(0, high=2, size=(self.states, self.ncols), dtype=np.int8)
 			# im_state = np.random.randint(0, high=2, size=(self.states, self.ncols)).astype(np.int8)
 			transition_action = np.empty((self.states, self.choices), dtype=np.uint16)
 			transition_next_state = np.empty((self.states, self.choices), dtype=np.uint16)
@@ -78,8 +79,8 @@ class Fast_bundle_empirical():
 			transition_state = np.repeat(np.arange(self.states), self.choices)
 			transition_action = transition_action.flatten()
 			transition_next_state = transition_next_state.flatten()
-			# address = np.logical_xor(im_state[transition_state], im_action[transition_action])
-			address = im_state[transition_state] * im_action[transition_action]
+			address = np.logical_xor(im_state[transition_state], im_action[transition_action])
+			# address = im_state[transition_state] * im_action[transition_action]
 			assert transition_state.size == num_transitions
 			assert transition_action.size == num_transitions
 			assert transition_next_state.size == num_transitions
@@ -92,38 +93,64 @@ class Fast_bundle_empirical():
 				rows, column_indices = np.ogrid[:address.shape[0], :address.shape[1]]
 				column_indices = column_indices - transition_state[:, np.newaxis]
 				address = address[rows, column_indices]
-			# data = np.logical_xor(address, np.roll(im_state[transition_next_state], 1, axis=1))*2-1
-			data = address * np.roll(im_state[transition_next_state], 1, axis=1)
+			data = np.logical_xor(address, np.roll(im_state[transition_next_state], 1, axis=1))*2-1
+			# data = address * np.roll(im_state[transition_next_state], 1, axis=1)
 			# import pdb; pdb.set_trace()
 			contents = np.sum(data, axis=0, dtype=np.int16)
 			if self.binarize_counters:
+				# binerize counters then use xor and hamming distance
 				contents[contents > 0] = 1
-				contents[contents < 0] = -1
-				random_plus_or_minus_one = rng.integers(0, high=2, size=self.ncols, dtype=np.int8)*2-1
-				mask = contents == 0
-				contents[mask] = random_plus_or_minus_one[mask]
-			# recall data from contents vector
-			for i in range(num_transitions):
-				# recalled_data = np.roll(np.logical_xor(address[i], recalled_vector), -1)
-				recalled_data = np.roll(address[i] * contents, -1)
-				# hamming_distances = np.count_nonzero(im_state[:,] != recalled_data, axis=1)
-				dot_products = np.sum(im_state[:,] * recalled_data, axis=1)
-				# self.match_hamming_counts[hamming_distances[transition_next_state[i]]] += 1
-				# self.distractor_hamming_counts[hamming_distances[(transition_next_state[i]+1) % self.states]] += 1 # a random distractor
-				# two_smallest = np.argpartition(hamming_distances, 2)[0:2]
-				two_largest = np.argpartition(dot_products , -2)[-2:]
-				if dot_products[two_largest[0]] > dot_products[two_largest[1]]:
-					if transition_next_state[i] != two_largest[0]:
+				contents[contents < 0] = 0
+				# random_plus_or_minus_one = rng.integers(0, high=2, size=self.ncols, dtype=np.int8)*2-1
+				# mask = contents == 0
+				# contents[mask] = random_plus_or_minus_one[mask]
+				for i in range(num_transitions):
+					# recalled_data = np.roll(np.logical_xor(address[i], recalled_vector), -1)
+					recalled_data = np.roll(np.logical_xor(address[i], contents), -1)
+					hamming_distances = np.count_nonzero(im_state[:,] != recalled_data, axis=1)
+					# dot_products = np.sum(im_state[:,] * recalled_data, axis=1)
+					# self.match_hamming_counts[hamming_distances[transition_next_state[i]]] += 1
+					# self.distractor_hamming_counts[hamming_distances[(transition_next_state[i]+1) % self.states]] += 1 # a random distractor
+					two_smallest = np.argpartition(hamming_distances, 2)[0:2]
+					# two_largest = np.argpartition(dot_products , -2)[-2:]
+					if hamming_distances[two_smallest[0]] < hamming_distances[two_smallest[1]]:
+						if transition_next_state[i] != two_smallest[0]:
+							fail_counts[epoch_id] += 1
+							fail_count += 1
+					elif hamming_distances[two_smallest[1]] < hamming_distances[two_smallest[0]]:
+						if transition_next_state[i] != two_smallest[1]:
+							fail_counts[epoch_id] += 1
+							fail_count += 1
+					elif self.count_multiple_matches_as_error or transition_next_state[i] != two_smallest[0]:
 						fail_counts[epoch_id] += 1
 						fail_count += 1
-				elif dot_products[two_largest[1]] > dot_products[two_largest[0]]:
-					if transition_next_state[i] != two_largest[1]:
+					trial_count += 1
+			else:
+				# import pdb; pdb.set_trace()
+				# non-thresholded counter, use dot product
+				address = address*2-1    # convert address to +1 / -1
+				im_state = im_state*2-1  # convert im_state to +1 / -1
+				for i in range(num_transitions):
+					# recalled_data = np.roll(np.logical_xor(address[i], recalled_vector), -1)
+					recalled_data = np.roll(-address[i] * contents, -1)  # - included so product has same result as xor
+					# hamming_distances = np.count_nonzero(im_state[:,] != recalled_data, axis=1)
+					dot_products = np.sum(im_state[:,] * recalled_data, axis=1) # *(-1)  # -1 not needed because of minus above
+					# self.match_hamming_counts[hamming_distances[transition_next_state[i]]] += 1
+					# self.distractor_hamming_counts[hamming_distances[(transition_next_state[i]+1) % self.states]] += 1 # a random distractor
+					# two_smallest = np.argpartition(hamming_distances, 2)[0:2]
+					two_largest = np.argpartition(dot_products , -2)[-2:]
+					if dot_products[two_largest[0]] > dot_products[two_largest[1]]:
+						if transition_next_state[i] != two_largest[0]:
+							fail_counts[epoch_id] += 1
+							fail_count += 1
+					elif dot_products[two_largest[1]] > dot_products[two_largest[0]]:
+						if transition_next_state[i] != two_largest[1]:
+							fail_counts[epoch_id] += 1
+							fail_count += 1
+					elif self.count_multiple_matches_as_error or transition_next_state[i] != two_largest[0]:
 						fail_counts[epoch_id] += 1
 						fail_count += 1
-				elif self.count_multiple_matches_as_error or transition_next_state[i] != two_largest[0]:
-					fail_counts[epoch_id] += 1
-					fail_count += 1
-				trial_count += 1
+					trial_count += 1
 		# print("fail counts are %s" % fail_counts)
 		assert np.sum(fail_counts) == fail_count
 		assert trial_count == (num_transitions * self.epochs)
@@ -143,18 +170,33 @@ class Fast_bundle_empirical():
 		# print("fast_sdm_empirical epochs=%s, perr=%s, std_error=%s" % (self.epochs, perr, self.std_error))
 
 
+	# output for bundle sizes:
+	# 	Bundle sizes, k=1000, d=100:
+	# 1 - 24002
+	# 2 - 40503
+	# 3 - 55649
+	# 4 - 70239
+	# 5 - 84572
+	# 6 - 98790
+	# 7 - 112965
+	# 8 - 127134
+	# 9 - 141311
+
 def main():
 	# from 8-bit bundle, perr=3, width = 62919		[3, 62919, 300],
 	# 	[2, 54000, 200],
-	# ncols = 62919; actions=10; choices=10; states=100  # should give error of 10e-3
-	ncols = 54000; actions=10; choices=10; states=100  # should give error of 10e-2
-	ncols = 4000; actions=10; choices=10; states=100  # should give error of 10e-2
-	binarize_counters=False; epochs=3
-	fbe = Fast_bundle_empirical(ncols, actions=10, states=100, choices=10, epochs=epochs,
-			count_multiple_matches_as_error=True, roll_address=True, debug=False,
+	actions=10; choices=10; states=100
+	# actions=3; choices=1; states=3; ncols=16	# for testing
+	# ncols = 62919; # should give error of 10e-3
+	# ncols = 55649;   # should give error of 10e-3; 3 - 55649 for binarized bundle
+	ncols = 40503  # should be error 10e-2 for binarized bundle
+	binarize_counters=False; epochs=10; roll_address=True
+	fbe = Fast_bundle_empirical(ncols, actions=actions, states=states, choices=choices, epochs=epochs,
+			count_multiple_matches_as_error=True, roll_address=roll_address, debug=False,
 			binarize_counters=binarize_counters)
-	print("binarize_counters=%s, epochs=%s, ncols=%s, mean_error=%s, std_error=%s" % (binarize_counters,
-		epochs, ncols, fbe.mean_error, fbe.std_error))
+	print("actions=%s, states=%s, choices=%s, binarize_counters=%s,"
+		" epochs=%s, roll_address=%s, ncols=%s, mean_error=%s, std_error=%s" % (actions, states, choices,
+		binarize_counters, epochs, roll_address, ncols, fbe.mean_error, fbe.std_error))
 
 if __name__ == "__main__":
 	main()
