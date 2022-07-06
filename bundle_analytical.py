85cef09b2950964852fbf75fcfc2971919a8f715
jeff Teeters <jeff@teeters.us>
2022-07-06 09:45:38 -0500

    add gallant size calculations to bundle_analytical

--- bundle_analytical.py
+++ bundle_analytical.py
@@ -2,6 +2,9 @@
 import scipy
 from scipy.stats import binom
 import numpy as np
+from scipy.stats import norm
+import math
+from scipy import special
 
 
 #Compute expected Hamming distance
@@ -41,3 +44,31 @@ def p_error_binom (N, D, dp_hit):
 		dhg -= distractor_hammings[k]
 		p_err += match_hammings[k] * (1.0 - dhg ** num_distractors)
 	return p_err
+
+
+def gallen(s, per):
+	# calculate required length using equation in Gallant paper
+	# solved to give length.  (Page 39: "This is equivalent to the probability
+	# of a random vector ["per"] being negative when selected from
+	# NormalDistibution(D, √((2S-1)D)" solved for D in terms of per and s 
+	return round(2*(-1 + 2*s)*special.erfcinv(2*per)**2)
+
+def gallenf(d, k, perf):
+	# bundle length from formula in Gallant paper from final probability error
+	# (perf) taking into account number of items in bundle (k) and size of item memory (d)
+	per = perf/(k*(d-1))
+	return gallen(k, per)
+
+def galerr(d, k, w):
+	# return expected error of Gallant bundle (not thresholded).
+	# d - number of items in item memory (related to "n" in gallant paper, d = n + 1)
+	# k - number of items stored in bundle ("s" in gallant paper)
+	# w - width (number of components) in bundle, e.g. 10,000
+	# calculated from page 39
+	# T (Z) = T (√ (D/(2S-1))).
+	tz = norm.cdf(-math.sqrt(w/(2*k-1)))
+	perr = np.exp((d-1)*k*np.log(1-tz))
+	return perr
+
+
+	# d items combined into trace vector and n items 
