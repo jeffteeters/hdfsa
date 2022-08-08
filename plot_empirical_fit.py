ef49f066be7d855b7cbf81344cc65f8251ce9530
jeff Teeters <jeff@teeters.us>
2022-08-07 21:17:30 -0700

    got working for initial plot

--- plot_empirical_fit.py
+++ plot_empirical_fit.py
@@ -56,50 +56,63 @@ def get_bundle_perr(mi, ie):
 	# mi entry in mem_info array (key-value pairs)
 	# ip is desired error, range 1 to 9 (10^(-ie))
 	global mdims
+	assert mi["mtype"] == "bundle"
+	name = mi["name"]
+	dims = mdims[name]
+	size = dims[ie - 1]
+	assert size[0] == ie, "First component of %s dims does not match error %s" % (name, ie)
+	ncols = size[1]
+	binarize_counters = mi["binarize_counters"]
 	epochs = get_epochs(ie, bundle=True)
 	if epochs is None:
 		# requires too many epochs, don't run
 		mean_error = math.nan
 		std_error = math.nan
+		clm_error = math.nan
 	else:
-		assert mi["mtype"] == "bundle"
-		name = mi["name"]
-		dims = mdims[name]
-		size = dims[ie - 1]
-		assert size[0] == ie, "First component of %s dims does not match error %s" % (name, ie)
-		ncols = size[1]
-		binarize_counters = mi["binarize_counters"]
-		fbe = Fast_bundle_empirical.Fast_bundle_empirical(ncols, epochs=epochs, binarize_counters=binarize_counters)
+		fbe = fast_bundle_empirical.Fast_bundle_empirical(ncols, epochs=epochs, binarize_counters=binarize_counters)
 		mean_error = fbe.mean_error
 		std_error = fbe.std_error
-	return (mean_error, std_error)
+		clm_error = std_error / math.sqrt(epochs) * 1.96  # 95% confidence interval for the mean (CLM)
+		print("%s, ie=%s, epochs=%s, error mean=%s, std=%s, clm=%s" % (name, ie, epochs, mean_error, std_error, clm_error))
+	mi["results"]["sizes"].append(ncols)
+	mi["results"]["mean_errors"].append(mean_error)
+	mi["results"]["std_errors"].append(std_error)
+	mi["results"]["clm_errors"].append(clm_error)
 
 def get_sdm_perr(mi, ie):
 	# return mean and standard deviation of error
 	# mi entry in mem_info array (key-value pairs)
 	# ie is desired error, range 1 to 9 (10^(-ie))
 	global mdims
+	assert mi["mtype"] == "sdm"
+	name = mi["name"]
+	dims = mdims[name]
+	size = dims[ie - 1]
+	assert size[0] == ie, "First component of %s dims does not match error %s" % (name, ie)
+	nrows = size[1]
+	nact = size[2]
+	bits_per_counter = mi["bits_per_counter"]
+	assert mi["match_method"] in ("hamming", "dot")
 	epochs = get_epochs(ie, bundle=False)
 	if epochs is None:
 		# requires too many epochs, don't run
 		mean_error = math.nan
 		std_error = math.nan
+		clm_error = math.nan
 	else:
-		assert mi["mtype"] == "sdm"
-		name = mi["name"]
-		dims = mdims[name]
-		size = dims[ie - 1]
-		assert size[0] == ie, "First component of %s dims does not match error %s" % (name, ie)
-		nrows = size[1]
-		nact = size[2]
-		bits_per_counter = mi["bits_per_counter"]
-		assert mi["match_method"] in ("hamming", "dot")
 		threshold_sum = mi["match_method"] == "hamming"
-		fse = Fast_sdm_empirical.Fast_sdm_empirical(nrows, ncols, nact, epochs=epochs,
+		ncols = 512
+		fse = fast_sdm_empirical.Fast_sdm_empirical(nrows, ncols, nact, epochs=epochs,
 			bits_per_counter=bits_per_counter, threshold_sum=threshold_sum)
 		mean_error = fse.mean_error
 		std_error = fse.std_error
-	return (mean_error, std_error)
+		clm_error = std_error / math.sqrt(epochs) * 1.96  # 95% confidence interval for the mean (CLM)
+		print("%s, ie=%s, epochs=%s, error mean=%s, std=%s, clm=%s" % (name, ie, epochs, mean_error, std_error, clm_error))
+	mi["results"]["sizes"].append(nrows)
+	mi["results"]["mean_errors"].append(mean_error)
+	mi["results"]["std_errors"].append(std_error)
+	mi["results"]["clm_errors"].append(clm_error)
 
 def get_epochs(ie, bundle=False):
 	# ie is expected error rate, range 1 to 9 (10^(-ie))
@@ -107,9 +120,11 @@ def get_epochs(ie, bundle=False):
 	num_transitions = 1000  # 100 states, 10 choices per state
 	desired_fail_count = 100
 	minimum_fail_count = 40
-	epochs_max = 1000 if not bundle else 200  # bundle takes longer, so use fewer epochs 
-	expected_perr = 10^(-ie)  # expected probability of error
-	desired_epochs = round(desired_fail_count / (expected_perr *num_transitions))
+	epochs_max = 1000 if not bundle else 5  # bundle takes longer, so use fewer epochs 
+	expected_perr = 10**(-ie)  # expected probability of error
+	desired_epochs = max(round(desired_fail_count / (expected_perr *num_transitions)), 2)
+	print("ie=%s, desired_epochs=%s, desired_fail_count=%s, expected_perr=%s" % (
+		ie, desired_epochs, desired_fail_count, expected_perr))
 	if desired_epochs <= epochs_max:
 		return desired_epochs
 	minimum_epochs = round(minimum_fail_count / (expected_perr *num_transitions))
@@ -123,111 +138,22 @@ def plot_fit(mtype="sdm"):
 	get_perr = get_sdm_perr if mtype=="sdm" else get_bundle_perr
 	for mi in mem_info:
 		if mi["mtype"] == mtype:
-			results = [get_perr(mi, ie) for ie in range(1, 9)]
-			mi["results"] = results
+			mi["results"] = {"sizes":[], "mean_errors":[], "std_errors":[], "clm_errors":[]}
+			for ie in range(1, 10):
+				get_perr(mi, ie) # calculates and appends to variables above in "results"
 	# now make plot
-
-
-
-
-
-	epochs_needed = round(minimum_fail_count / (expected_perr *num_transitions))
-			if epochs_needed > epochs_start:
-				if epochs_needed > epochs_max:
-					print("num_steps=%s, nrows=%s, expected_perr=%s, epochs_needed=%s; stopping because epochs_max=%s" % (
-						num_steps, nrows, expected_perr, epochs_needed, epochs_max))
-					break
-				epochs = epochs_needed
-
-
-
-def optimum_nact(k, m):
-	# return optimum nact given number items stored (k) and number of rows (m)
-	# from formula given in Pentti paper
-	fraction_rows_activated =  1.0 / ((2*m*k)**(1/3))
-	nact = round(m * fraction_rows_activated)
-	return nact
-
-def predict_sdm_dims(kcon, mcon, k):
-	# predict sdm dimensions for each perr power of 10 assuming perr = kcon*10**(mcon*x),
-	# or log10(perr) = log10(kcon) + mcon*x
-	# or x = nrows = (log10(perr) - log10(kcon)) / mcon
-	# k is number of items stored in the sdm (used to compute nact from nrows)
-	dims = []
-	for i in range(1,10):
-		log_perr = -i
-		log_kcon = np.log10(kcon)
-		nrows = round((log_perr - log_kcon) / mcon)
-		nact = optimum_nact(k, nrows)
-		dims.append([i, nrows, nact])
-	return dims
-
-
-def main():
-	ncols = 512; actions=10; choices=10; states=100
-	k = 1000
-	d = 100
-	num_transitions = choices * states
-	epochs=50
-	threshold_sum = False
-	bits_per_counter=8
-	row_start = 20
-	row_step = 10
-	epochs_start = 40
-	minimum_fail_count = 40
-	epochs_max = 1000
-	# wanted_rows = range(40, 121, 20)
-	max_num_steps = 100
-	perr = np.empty(max_num_steps, dtype=np.float64)
-	rows = np.empty(max_num_steps, dtype=np.float64)
-	nacts = np.empty(max_num_steps, dtype=np.int16)
-	confidence = np.empty(max_num_steps, dtype=np.float64)  # confidence interval
-	nrows = row_start
-	expected_perr = None
-	for num_steps in range(max_num_steps):  # number of steps saved
-		if num_steps >= 2:
-			expected_perr = perr[num_steps-1]**2 / perr[num_steps -2]  # expected next perr
-			epochs_needed = round(minimum_fail_count / (expected_perr *num_transitions))
-			if epochs_needed > epochs_start:
-				if epochs_needed > epochs_max:
-					print("num_steps=%s, nrows=%s, expected_perr=%s, epochs_needed=%s; stopping because epochs_max=%s" % (
-						num_steps, nrows, expected_perr, epochs_needed, epochs_max))
-					break
-				epochs = epochs_needed
-		nact = optimum_nact(k, nrows)
-		if nact > 1:
-			print("stopping because nact > 1, nrows=%s" % nrows)
-			break
-		print("num_steps=%s, nrows=%s, nact=%s, expected_perr=%s, epochs=%s" % (num_steps,
-			nrows, nact, expected_perr, epochs))
-		nacts[num_steps] = nact
-		rows[num_steps] = nrows
-		fse = fast_sdm_empirical.Fast_sdm_empirical(nrows, ncols, nact, actions=actions, states=states, choices=choices,
-			threshold_sum=threshold_sum, bits_per_counter=bits_per_counter, epochs=epochs)
-		perr[num_steps] = fse.mean_error
-		confidence[num_steps] = (fse.std_error / math.sqrt(epochs)) * 1.96  # 95% confidence interval for the mean (CLM)
-		nrows += row_step
-		print("epochs=%s, sdm size=%s/%s, perr=%s, std=%s" %(epochs, nrows, nact, fse.mean_error, fse.std_error))
-
-	# fit a line, log10(y) = kcon * 10 ** (mcon * nrows).  Find kcon and mcon  ("con" means constant)
-	x = rows[0:num_steps]
-	y = perr[0:num_steps]
-	ylog = np.log10(y)
-	result = linregress(x, y=ylog)
-	kcon = 10.0**(result.intercept)
-	mcon = result.slope
-	assert mcon < 1
-	yreg = kcon * 10.0**(mcon *x)
-	print("linregress for err=k*exp(-m*x), k=%s, m=%s" % (kcon, mcon))
-	dims = predict_sdm_dims(kcon, mcon, k)
-	print("predicted sdm_dims for different perr powers of 10 is:\n%s" % dims)
-
-
-	# make plot
-	plt.errorbar(x, y, yerr=confidence[0:num_steps], fmt="-o", label="non-thresholded")
-	plt.errorbar(x, yreg, yerr=None, fmt="-", label="line_fit")
-	plt.title("sdm error vs size, threshold_sum=%s, bits_per_counter=%s" % (threshold_sum, bits_per_counter))
-	plt.xlabel("sdm num rows")
+	desired_errors = [10**(-i) for i in range(1, 10)]
+	for mi in mem_info:
+		if mi["mtype"] == mtype:
+			name = mi["name"]
+			x = mi["results"]["sizes"]
+			y = mi["results"]["mean_errors"]
+			ybar = mi["results"]["clm_errors"]
+			plt.errorbar(x, y, yerr=ybar, fmt="-o", label=name)
+			plt.errorbar(x, desired_errors, yerr=None, fmt="o", label="%s - Desired error" % name)
+	plt.title("%s empirical vs. desired error" % mtype)
+	xlabel = "sdm num rows" if mtype == "sdm" else "Superposition vector width"
+	plt.xlabel("xlabel")
 	plt.ylabel("Fraction error")
 	plt.yscale('log')
 	# xlabels = ["%s/%s" % (rows[i], nacts[i]) for i in range(num_steps)]
@@ -235,6 +161,11 @@ def main():
 	plt.grid()
 	plt.legend(loc='upper right')
 	plt.show()
+
+def main():
+	plot_fit("sdm")
+	plot_fit("bundle")
+
 
 
 if __name__ == "__main__":
